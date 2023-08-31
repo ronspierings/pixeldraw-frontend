0a8c0b61fced2d9f5a40601602fbad15982186a5
Ron Spierings <rjl.spierings@avans.nl>
2023-08-31 11:14:19 +0200

    Initial commit

---
+++ src/components/GridView.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container class="fill-height bg-red" fluid>
+        <v-responsive width="100%"></v-responsive>
+
+        <v-col v-for="pixel in pixels" :key="pixel">
+            <v-row fluid :style="{ backgroundColor: pixel._color, height: '100px', width: '100px' }">
+                {{ pixel._color }}
+            </v-row>
+        </v-col>
+    </v-container>
+</template>
+
+<script>
+    import PixelData from '@/classes/pixelData.js'
+
+    export default {
+        data () 
+            {
+            return {
+                pixels: [
+                    new PixelData("#CCC"),
+                    new PixelData("#DDD"),
+                    new PixelData("#123"),
+                    new PixelData("#EEE")
+                ]
+            }
+        }        
+    }
+</script>@/classes/PixelData.js
